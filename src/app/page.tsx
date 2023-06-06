53bae70651079026f1adc1b76916b95252924b14
bovdaboat <ignatgabov@gmail.com>
2023-06-05 22:25:03 -0700

    finished doing the page.tsx and ready to do the commp of the app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,26 @@
-export default function Home() {
+import Link from "next/link";
+import {prisma} from "@/db";
+
+export default async function Home() {
+
+  const todo = await prisma.todo.findMany()
     return (
         <>
-        <header>
+        <header className="flex justify-between items-center mb-4 items-center">
             <h1 className="text-2xl">
                 Todo
             </h1>
+            <Link
+                className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700
+                focus-within:bg-slate-700 outline-none"
+                href="/new">
+                New
+            </Link>
         </header>
-        <ul>
-
+        <ul className="pl-4">
+            {todo.map(todo => (
+                <TodoItem key={todo.id} {...todo} />
+            ))}
         </ul>
         </>
     )
