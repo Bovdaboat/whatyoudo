cf71c50f1925afda59506efb543f4b8c1c4d0682
bovdaboat <ignatgabov@gmail.com>
2023-06-06 22:12:23 -0700

    finished the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,24 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css"
+import { Inter } from "next/font/google"
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: 'What you do app',
-  description: 'it a to do app',
+  title: "Todo App",
 }
 
 export default function RootLayout({
-  children,
-}: {
+                                     children,
+                                   }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} bg-slate-800 text-slate-100 container mx-auto 
-       p-4`}>{children}</body>
-    </html>
+      <html lang="en">
+      <body
+          className={`${inter.className} bg-slate-800 text-slate-100 container mx-auto p-4`}
+      >
+      {children}
+      </body>
+      </html>
   )
 }
