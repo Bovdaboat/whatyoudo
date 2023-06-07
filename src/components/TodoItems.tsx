4fa5c049c8380d14d6cd221260665b955eed98fe
bovdaboat <ignatgabov@gmail.com>
2023-06-06 21:56:06 -0700

    making the list fuctional and added the tailwind css to the main page

--- src/components/TodoItems.tsx
+++ src/components/TodoItems.tsx
@@ -1,17 +1,28 @@
+"use client"
 
-
-type TodoItemsProps = {
-id: string
-title: string
-complete: Boolean
+type TodoItemProps = {
+    id: string
+    title: string
+    complete: boolean
+    toggleTodo: (id: string, complete: boolean) => void
 }
 
-export function TodoItems ({ id, title, complete }: TodoItemsProps) {
+export function TodoItem({ id, title, complete, toggleTodo }: TodoItemProps) {
     return (
         <li className="flex gap-1 items-center">
-            <input id={id} type={"checkbox"} className="cursor-pointer peer"/>
-            <label htmlFor={id} className="cursor-pointer peer-checked:line-through">
+            <input
+                id={id}
+                type="checkbox"
+                className="cursor-pointer peer"
+                defaultChecked={complete}
+                onChange={e => toggleTodo(id, e.target.checked)}
+            />
+            <label
+                htmlFor={id}
+                className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500"
+            >
                 {title}
             </label>
-    </li>
-    )}
+        </li>
+    )
+}
